019e4746baf832ceeae94cb70126c21efaf9f282
DipanshuSehgal270 <dipanshusehgal4@gmail.com>
2025-12-10 20:55:52 +0530

    security commit

--- src/main/java/com/example/Book/Management/System/healthChecks/AuthorDbHealthIndicator.java
+++ src/main/java/com/example/Book/Management/System/healthChecks/AuthorDbHealthIndicator.java
@@ -1,30 +1,30 @@
-package com.example.Book.Management.System.healthChecks;
-
-import com.example.Book.Management.System.repository.AuthorRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.actuate.health.HealthIndicator;
-import org.springframework.stereotype.Component;
-
-@Component
-public class AuthorDbHealthIndicator implements HealthIndicator {
-
-    @Autowired
-    private AuthorRepository authorRepository;
-
-    @Override
-    public Health health() {
-        try{
-            Long authorCount = authorRepository.count();
-
-            return Health.up().withDetail("Author Count" , authorCount)
-                    .withDetail("repositoryStatus" , "Connected and Operational")
-                    .build();
-
-        } catch (Exception e) {
-
-            return Health.down().withDetail("repositoryStatus" , "Connection Failure")
-                    .withDetail("Error" , e.getMessage()).build();
-        }
-    }
-}
+//package com.example.Book.Management.System.healthChecks;
+//
+//import com.example.Book.Management.System.repository.AuthorRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.actuate.health.Health;
+//import org.springframework.boot.actuate.health.HealthIndicator;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class AuthorDbHealthIndicator implements HealthIndicator {
+//
+//    @Autowired
+//    private AuthorRepository authorRepository;
+//
+//    @Override
+//    public Health health() {
+//        try{
+//            Long authorCount = authorRepository.count();
+//
+//            return Health.up().withDetail("Author Count" , authorCount)
+//                    .withDetail("repositoryStatus" , "Connected and Operational")
+//                    .build();
+//
+//        } catch (Exception e) {
+//
+//            return Health.down().withDetail("repositoryStatus" , "Connection Failure")
+//                    .withDetail("Error" , e.getMessage()).build();
+//        }
+//    }
+//}
